d06e66e660b3688811451e363b3e163e82496fda
MB <malekbar9808@gmail.com>
2026-01-13 11:47:31 -0500

    Begun Creating Search Script
    Created the python file for the script and the requirements file that outlines the required packages

---
+++ Search.py
@@ -0,0 +1,12 @@
+'''
+Get prompt from website
+Get and apply filters to prompt
+Search websites
+Get data
+Store data on sql database
+
+-- Helpful vid https://youtu.be/uWg-XAyyJTk?si=zZXdbLTFMwEQ2jHW
+'''
+import requests
+from bs4 import BeautifulSoup
+
