af95ff8a96abb48c725225d3e2e1c72e798a5250
MB <malekbar9808@gmail.com>
2026-01-15 11:52:03 -0500

    Added functionality to search file.
    Allowed the search file to create and attach itself to a temporary database.
    Edited the gitignore file to ignore the database file
    Database will be changed from sqlite to postgresql once the website/db is hosted on Fly.io

--- Search.py
+++ Search.py
@@ -9,4 +9,17 @@ Store data on sql database
 '''
 import requests
 from bs4 import BeautifulSoup
+from pony import orm
 
+db = orm.Database()
+#temporarily going to use sqlite to gauruntee functionality 
+#going to switch to postgres once website is hosted.
+db.bind(provider='sqlite', filename='products.db', create_db=True)
+#db.bind(provider='postgres', user='Admin', password='Admin', host='local', port='1127')
+
+#initializing the product class, to be changed once we have an actual database set up
+class Product(db.Entity):
+    name=orm.Required(str)
+    price=orm.Required(float)
+
+db.generate_mapping(create_tables=True)
