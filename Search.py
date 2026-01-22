9753b34ce3a739e97ff0cc57c0f6968c359e1479
RohanB14 <rohanbetton14221@gmail.com>
2026-01-22 12:09:56 -0500

    Newegg Webscrape

--- Search.py
+++ Search.py
@@ -7,6 +7,15 @@ Store data on sql database
 
 
 '''
-import requests
-from bs4 import BeautifulSoup
 
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.newegg.com/gigabyte-windforce-gv-n5070wf3oc-12gd-geforce-rtx-5070-12gb-graphics-card-triple-fans/p/N82E16814932777"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+
+prices = doc.find_all(text="$")
+parent = prices[0].parent
+strong = parent.find("strong")
+print(strong.string)
